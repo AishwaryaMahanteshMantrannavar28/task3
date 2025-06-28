e329c29ad6f5028bf07ca9dc2579ba367bd38deb
AishwaryaMahanteshMantrannavar28 <aishwaryamantrannavara@gmail.com>
2025-06-28 20:09:58 +0530

    Add files via upload

---
+++ task3.sql
@@ -0,0 +1,10 @@
+-- Task 3: Writing Basic SELECT Queries
+ --  1. SELECT 
+    --  Select * from  members;
+ --    2.WHERE
+--   select * from members where Member_id=121;
+-- 3.  ORDER BY
+--   select name from members order by Member_id;
+-- 4. LIMIT
+
+  
